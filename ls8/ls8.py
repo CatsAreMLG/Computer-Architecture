ac0279d6421dd9d38030cc121021a9495c46d65c
CatsAreMLG <joshuadgon@gmail.com>
2019-11-02 05:18:51 -0500

    Finish cpu.py

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,9 +3,11 @@
 """Main."""
 
 import sys
+import os
 from cpu import *
 
 cpu = CPU()
+path = os.path.dirname(os.path.realpath(__file__)) + '/examples/print8.ls8'
 
-cpu.load()
+cpu.load(path)
 cpu.run()
